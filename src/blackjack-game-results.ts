512327aabf131fd5b0372091e4018cc8e4920dc5
Austin Brown <austinnbrown95@gmail.com>
2023-12-04 18:46:07 -0600

    fixed TS errors durationFormatter
    Generic <string>

--- src/blackjack-game-results.ts
+++ src/blackjack-game-results.ts
@@ -1,4 +1,10 @@
+import { durationFormatter } from 'human-Readable';
 
+const format = durationFormatter<string>();
+
+const justDaysFormat = durationFormatter<string> ({
+    allowMultiples: ["y", "mo", "d"]
+});
 export type GameResult = {
     won: boolean;
     start: string;
@@ -28,3 +34,32 @@ export const getWinningPercentageDisplay = (results: GameResult[]): WinningPerce
     };
 };
 
+
+
+export interface GeneralGameTimeFactsDisplay {
+    lastPlayed: string; // milliseconds for now, but "display" implies human-readable...
+    shortestGame: string;
+    longestGame: string;
+};
+
+const getGeneralGameTimeFacts = (
+    results: GameResult[]
+    , fromDateMilliseconds: number 
+): GeneralGameTimeFactsDisplay => {
+
+    const gameEndDatesInMilliseconds = results
+        .map(x => Date.parse(x.end))
+        .filter(x => x <= fromDateMilliseconds)
+    ;
+
+    const gameDurationsInMilliseconds = results
+        .filter(x => Date.parse(x.end) <= fromDateMilliseconds)
+        .map(x => Date.parse(x.end) - Date.parse(x.start))
+    ;
+
+    return {
+        lastPlayed: justDaysFormat(fromDateMilliseconds - Math.max(...gameEndDatesInMilliseconds))
+        , shortestGame: format(Math.min(...gameDurationsInMilliseconds))
+        , longestGame: format(Math.max(...gameDurationsInMilliseconds))
+    };
+};
